057cb2ee825863c30f8f1f5114782181724e16a3
Samyak Jain <samyaksjjainsj@gmail.com>
2018-09-16 19:52:17 +0530

    Added registration

---
+++ register.js
@@ -0,0 +1,13 @@
+function submit() {
+    
+    var user_email = document.getElementById("email");
+    var user_pass = document.getElementById("pass");
+    var user_cpass = document.getElementById("cpass");
+    var user_school = document.getElementById("school");
+    var user_address = document.getElementById("add");
+    var user_teacher = document.getElementById("teacher");
+    var user_phone = document.getElementById("phone");
+    
+    console.log (user_address);
+    
+}
